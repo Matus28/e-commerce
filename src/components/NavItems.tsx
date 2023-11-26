bac178815a9bc6ed4b4714f21b26e2c232cde7b3
Matus Tatarko <matus.tatarko28@gmail.com>
2023-11-26 18:04:54 +0100

    feat: creates navigation bar for the desktop version of the app

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { PRODUCT_CATEGORIES } from "@/config";
+import * as React from "react";
+import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/useOnClickOutside";
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = React.useState<null | number>(null);
+
+  React.useEffect(() => {
+    const handler = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+
+    document.addEventListener("keydown", handler);
+
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
+
+  const isAnyOpen = activeIndex !== null;
+
+  const navRef = React.useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
+  return (
+    <div className="flex gap-4 h-full" ref={navRef}>
+      {PRODUCT_CATEGORIES.map((category, index) => {
+        const handleOpen = () => {
+          if (activeIndex === index) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(index);
+          }
+        };
+
+        const isOpen = index === activeIndex;
+
+        return (
+          <NavItem
+            key={category.value}
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavItems;
