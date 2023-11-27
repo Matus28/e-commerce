2ae679ddc9f4f1b5c975e1b54c81bdc63eca4406
Matus Tatarko <matus.tatarko28@gmail.com>
2023-11-27 13:25:32 +0100

    refactor: fixing payload logger problem for now

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,31 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { getPayloadClient } from "./getPayload";
+import { nextApp, nextHandler } from "./next-utils";
 
 const app = express();
-
 const PORT = Number(process.env.PORT) || 3000;
+
+const start = async (): Promise<void> => {
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`);
+      },
+    },
+  });
+
+  app.use((req: Request, res: Response) => nextHandler(req, res));
+
+  nextApp.prepare().then(() => {
+    // payload.logger.info("Next.js started!");
+
+    app.listen(PORT, async () => {
+      // payload.logger.info(
+      // `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+      // );
+    });
+  });
+};
+
+start();
