53c586526d58ce4b602911bf65f11817b3531574
Matus Tatarko <matus.tatarko28@gmail.com>
2023-11-28 23:57:13 +0100

    feat: creates Providers components for providing gRTC and QueryClient services

---
+++ src/components/Providers.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import * as React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { trpc } from "@/trpc/client";
+import { httpBatchLink } from "@trpc/client";
+
+// React.PropsWithChildren === {children: ReactNode}
+const Providers = ({ children }: React.PropsWithChildren) => {
+  const [queryClient] = React.useState(() => new QueryClient());
+  const [trpcClient] = React.useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/trpc`,
+          fetch(url, options) {
+            return fetch(url, {
+              ...options,
+              credentials: "include",
+            });
+          },
+        }),
+      ],
+    })
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
+};
+
+export default Providers;
